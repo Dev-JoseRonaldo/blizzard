24895f8deb17b8ac81af139c55e0dc0e8d9f15e1
José Ronaldo <dev.joseronaldo@gmail.com>
2022-12-28 19:38:13 -0300

    refactor: customizing tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,28 @@ module.exports = {
     './src/layout/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Poppins, sans-serif',
+        secondary: 'Inter, sans-serif',
+      },
+      colors: {
+        primary: '#00AEFF',
+        primaryLight: '#61CDFF',
+        blue: {
+          600: '#148EFF',
+        },
+        gray: {
+          500: '#E5E5E5',
+          600: '#9D9D9D',
+          700: '#8F9199',
+          800: '#15171B',
+        },
+        black: {
+          800: '#020203',
+        },
+      },
+    },
   },
   plugins: [],
 }
